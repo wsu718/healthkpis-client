e0bc207d1a646fa17391f1f6f21d0e6cd8785560
William Sulinski <w@williamsulinski.com>
2020-02-02 07:54:15 -0800

    Split client and API repos

--- API/data/seeds/01-sleep-scores.js
+++
@@ -1,7 +0,0 @@
-exports.seed = function (knex, Promise) {
-  return knex('sleep').insert([
-    { date: '2019-12-12', seconds: 0, durationHours: 7, durationMinutes: 45, score: 76, bedtime: '22:45' },
-    { date: '2019-12-13', seconds: 0, durationHours: 6, durationMinutes: 33, score: 76, bedtime: '22:45' }
-  ]);
-};
-
