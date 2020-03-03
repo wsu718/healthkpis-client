09fbb94b3255147a61044ecc0bf315c4a810894e
William Sulinski <w@williamsulinski.com>
2020-03-03 10:12:52 -0800

    Add new data fields to SleepData and clean it up

--- src/components/SleepData.js
+++ src/components/SleepData.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getSleep } from '../actions/actions';
 import { useAuth0 } from "../react-auth0-spa";
+import moment from 'moment';
 
 const SleepData = props => {
     const [showResult, setShowResult] = useState(false);
@@ -37,26 +38,33 @@ const SleepData = props => {
 
 
         <div>
-            <h2>Your sleep data</h2>
-            {/* <button onClick={callApi}>Ping API</button> */}
-            {/* {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>} */}
+            <h2>Your health data</h2>
             <table className="datatable">
                 <thead>
                     <tr>
                         <th>Date</th>
-                        <th>Duration</th>
                         <th>Sleep score</th>
+                        <th>Duration</th>
                         <th>Bedtime</th>
+                        <th>Readiness</th>
+                        <th>HRV</th>
+                        <th>RHR</th>
+                        <th>Weight</th>
                     </tr>
                 </thead>
                 <tbody>
                     {sleepEntries.map((sleep, index) => (
                         <tr key={index}>
                             <td><Link to={`/sleep/${sleep.summary_date}`}>{sleep.summary_date.replace(/^(\d{4})-(\d{2})-(\d{2})$/, "$2-$3-$1")}</Link></td>
-                            <td>{sleep.duration}</td>
-                            <td>{sleep.user_id}</td>
                             <td>{sleep.score_total}</td>
-                            <td>{sleep.bedtime_start}</td>
+                            {/* <td>{sleep.duration}</td> */}
+                            {/* Moment works with milliseconds by default, so multiply what we store as seconds by 1000 */}
+                            <td>{moment.duration(sleep.duration * 1000).hours()}h {moment.duration(sleep.duration * 1000).minutes()}m</td>
+                            <td>{moment(sleep.bedtime_start).format('hh[:]mm A')}</td>
+                            <td>{sleep.readiness}</td>
+                            <td>{sleep.hrv}</td>
+                            <td>{sleep.rhr}</td>
+                            <td>{sleep.weight}</td>
                         </tr>
                     ))}
                 </tbody>
