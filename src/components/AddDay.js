86515766b0ae3905d6cd2771fb39b653a3b40e96
William Sulinski <w@williamsulinski.com>
2020-04-03 13:43:46 -0700

    Add error messages to AddDay

--- src/components/AddDay.js
+++ src/components/AddDay.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { useAuth0 } from "../react-auth0-spa";
+import { useAuth0 } from '../react-auth0-spa';
 import { useForm } from 'react-hook-form';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 import './AddDay.css';
 
 const AddDay = props => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const [durationHours, setDurationHours] = useState(0);
     const [durationMinutes, setDurationMinutes] = useState(0);
     const { getTokenSilently } = useAuth0();
@@ -29,7 +29,7 @@ const AddDay = props => {
         } catch (error) {
             console.error(error);
         }
-        history.push("/data");
+        history.push('/data');
     }
 
     const handleDurationChanges = e => {
@@ -46,109 +46,121 @@ const AddDay = props => {
                 <h2>
                     Add day
                 </h2>
-                <form className="adddayform" onSubmit={handleSubmit(onSubmit)}>
+                <form className='adddayform' onSubmit={handleSubmit(onSubmit)}>
                     <div className='form__input'>
-                        <label htmlFor="date">Date</label>
+                        <label htmlFor='date'>Date</label>
                         <input
-                            type="date"
-                            name="summary_date"
-                            ref={register}
-                            aria-label="Date"
-                            className="adddaydate"
+                            type='date'
+                            name='summary_date'
+                            ref={register({ required: true })}
+                            aria-label='Date'
+                            className='adddaydate'
                             defaultValue={new Date().toISOString().substr(0, 10)}
                         />
+                        {errors.summary_date && <p className='error-message'>This field is required.</p>}
                     </div>
                     <div className='form__input'>
-                        <label htmlFor="score">Sleep score</label>
+                        <label htmlFor='score'>Sleep score</label>
                         <input
-                            type="number"
-                            name="score_total"
-                            min="0"
-                            max="100"
-                            ref={register}
+                            type='number'
+                            name='score_total'
+                            min='0'
+                            max='100'
+                            ref={register({ required: true })}
                         />
+                        {errors.score_total && <p className='error-message'>This field is required.</p>}
+
+
                         <div className='form__input'>
-                            <label htmlFor="bedtime">Bedtime</label>
+                            <label htmlFor='bedtime'>Bedtime</label>
                             <input
-                                type="time"
-                                name="bedtime_start"
-                                id="bedtime"
-                                ref={register}
+                                type='time'
+                                name='bedtime_start'
+                                id='bedtime'
+                                ref={register({ required: true })}
                             />
+                            {errors.bedtime_start && <p className='error-message'>This field is required.</p>}
                         </div>
                         <div className='form__input'>
                             <label >Sleep duration</label>
-                            <div className="duration-container">
+                            <div className='duration-container'>
                                 <div>
                                     <input
-                                        type="number"
-                                        name="durationHours"
-                                        id="durationHours"
-                                        min="0"
-                                        max="24"
-                                        aria-label="Duration Hours"
+                                        type='number'
+                                        name='durationHours'
+                                        id='durationHours'
+                                        min='0'
+                                        max='24'
+                                        aria-label='Duration Hours'
                                         onChange={handleDurationChanges}
+                                        required
                                     />
-                                    <label htmlFor="durationHours">hrs</label>
+                                    <label htmlFor='durationHours'>hrs</label>
                                 </div>
                                 <div>
                                     <input
-                                        id="durationMinutes"
-                                        type="number"
-                                        name="durationMinutes"
-                                        min="0"
-                                        max="59"
-                                        aria-label="Duration Minutes"
+                                        id='durationMinutes'
+                                        type='number'
+                                        name='durationMinutes'
+                                        min='0'
+                                        max='59'
+                                        aria-label='Duration Minutes'
                                         onChange={handleDurationChanges}
+                                        required
                                     />
-                                    <label htmlFor="durationMinutes">
+                                    <label htmlFor='durationMinutes'>
                                         mins
                                     </label>
                                 </div>
                             </div>
                         </div>
                         <div className='form__input'>
-                            <label htmlFor="readiness">Readiness</label>
+                            <label htmlFor='readiness'>Readiness</label>
                             <input
-                                type="number"
-                                name="readiness"
-                                id="readiness"
-                                min="0"
-                                max="100"
-                                ref={register}
+                                type='number'
+                                name='readiness'
+                                id='readiness'
+                                min='0'
+                                max='100'
+                                ref={register({ required: true })}
                             />
+                            {errors.readiness && <p className='error-message'>This field is required.</p>}
+
                         </div>
                         <div className='form__input'>
-                            <label htmlFor="hrv">HRV avg</label>
+                            <label htmlFor='hrv'>HRV avg</label>
                             <input
-                                type="number"
-                                name="hrv"
-                                min="0"
-                                max="100"
-                                ref={register}
+                                type='number'
+                                name='hrv'
+                                min='0'
+                                max='100'
+                                ref={register({ required: true })}
                             />
+                            {errors.hrv && <p className='error-message'>This field is required.</p>}
                         </div>
                         <div className='form__input'>
-                            <label htmlFor="rhr">RHR avg</label>
+                            <label htmlFor='rhr'>RHR avg</label>
                             <input
-                                type="number"
-                                name="rhr"
-                                min="0"
-                                max="100"
-                                ref={register}
+                                type='number'
+                                name='rhr'
+                                min='0'
+                                max='100'
+                                ref={register({ required: true })}
                             />
+                            {errors.rhr && <p className='error-message'>This field is required.</p>}
                         </div>
                         <div className='form__input'>
-                            <label htmlFor="weight">Weight (lbs)</label>
+                            <label htmlFor='weight'>Weight (lbs)</label>
                             <input
-                                type="number"
-                                name="weight"
-                                min="0"
-                                max="500"
+                                type='number'
+                                name='weight'
+                                min='0'
+                                max='500'
                                 ref={register}
                             />
+
                         </div>
-                        <input type="submit" />
+                        <input type='submit' />
                     </div>
                 </form>
             </main>
--- src/components/SleepReview.js
+++ src/components/SleepReview.js
@@ -1,93 +1,91 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
+import React from 'react';
 
-// const SleepReview = props => {
+const SleepReview = props => {
 
-//     // const dayReviewing = props.match.params.sleepDate;
+    // get sleep data from props
 
-//     // // console.log(dayReviewing)
+    const dayReviewing = props.match.params.sleepDate;
 
-//     // // This Day's Sleep
-//     // const thisDaySleepDurationHours = props.sleepEntries.find(today => today.date === dayReviewing).durationHours
-//     // const thisDaySleepDurationMinutes = props.sleepEntries.find(today => today.date === dayReviewing).durationMinutes
-//     // const thisDaySleepDurationTotal = thisDaySleepDurationHours + (thisDaySleepDurationMinutes / 60)
-
-
-//     // const thisDaySleepScore = props.sleepEntries.find(today => today.date === dayReviewing).score
-
-
-//     // // Average Sleep Score
-//     // const averageSleepScore = Math.round(props.sleepEntries.reduce((acc, val) => acc + val.score, 0) / props.sleepEntriesLength)
-
-//     // // Average Sleep Duration: to calculate average sleep duration:
-
-//     // // 1. We need to sum the hours from all sleep entries 
-//     // const totalDurationHours = props.sleepEntries.reduce((acc, val) => acc + val.durationHours, 0)
-
-//     // // 2. Then we need to sum the minutes from all the sleep entries, and add them to the hours
-//     // const totalDurationMinutes = (props.sleepEntries.reduce((acc, val) => acc + val.durationMinutes, 0)) / 60
-//     // const totalDurationHoursAndMinutes = totalDurationHours + (totalDurationMinutes / 60)
-
-//     // // 3. Then we need to get an average 
-//     // const averageSleep = Math.round(totalDurationHoursAndMinutes / props.sleepEntriesLength);
-
-//     // // 3. But we also need to show the remaining minutes
-//     // const averageSleepMinRemain = Math.round(totalDurationHoursAndMinutes % props.sleepEntriesLength)
+    // console.log(dayReviewing)
 
 
 
-//     // // Change in sleep
-//     // const changeDuration = Math.round((thisDaySleepDurationTotal - averageSleep) / thisDaySleepDurationTotal * 100)
-//     // const changeScore = Math.round((thisDaySleepScore - averageSleepScore) / thisDaySleepScore * 100)
 
-//     return (
-//         <div>
-//             <h2>Data on your sleep</h2>
-
-//             {/* <p>{dayReviewing.replace(/^(\d{4})-(\d{2})-(\d{2})$/, "$2-$3-$1")}</p> */}
-
-//             <h3>{/* Add awards */}</h3>
-
-//             {/* <table className="datatable">
-//                 <thead>
-//                     <tr>
-//                         <th></th>
-//                         <th>Change</th>
-//                         <th>Last night</th>
-//                         <th>Average</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     <tr>
-//                         <th>Sleep duration</th>
-//                         <td>{changeDuration}%</td>
-//                         <td>{thisDaySleepDurationHours} hrs, {thisDaySleepDurationMinutes} min </td>
-//                         <td>{averageSleep} hrs, {averageSleepMinRemain} mins</td>
-//                     </tr>
-//                     <tr>
-//                         <th>Sleep score</th>
-//                         <td>{changeScore}%</td>
-//                         <td>{thisDaySleepScore}</td>
-//                         <td>{averageSleepScore}</td>
-//                     </tr>
-//                     <tr>
-//                         <th>Bedtime</th>
-//                         <td>-20%</td>
-//                         <td>22:59</td>
-//                         <td>21:00</td>
-//                     </tr>
-//                 </tbody>
-//             </table> */}
+    // // This Day's Sleep
+    // const thisDaySleepDurationHours = props.sleepEntries.find(today => today.date === dayReviewing).durationHours
+    // const thisDaySleepDurationMinutes = props.sleepEntries.find(today => today.date === dayReviewing).durationMinutes
+    // const thisDaySleepDurationTotal = thisDaySleepDurationHours + (thisDaySleepDurationMinutes / 60)
 
 
-//         </div>
-//     )
-// }
+    // const thisDaySleepScore = props.sleepEntries.find(today => today.date === dayReviewing).score
 
-// const mapStateToProps = state => {
-//     return {
-//         sleepEntriesLength: state.sleepEntries.length,
-//         sleepEntries: state.sleepEntries
-//     }
-// }
-// export default connect(mapStateToProps, {})(SleepReview);
+
+    // // Average Sleep Score
+    // const averageSleepScore = Math.round(props.sleepEntries.reduce((acc, val) => acc + val.score, 0) / props.sleepEntriesLength)
+
+    // // Average Sleep Duration: to calculate average sleep duration:
+
+    // // 1. We need to sum the hours from all sleep entries 
+    // const totalDurationHours = props.sleepEntries.reduce((acc, val) => acc + val.durationHours, 0)
+
+    // // 2. Then we need to sum the minutes from all the sleep entries, and add them to the hours
+    // const totalDurationMinutes = (props.sleepEntries.reduce((acc, val) => acc + val.durationMinutes, 0)) / 60
+    // const totalDurationHoursAndMinutes = totalDurationHours + (totalDurationMinutes / 60)
+
+    // // 3. Then we need to get an average 
+    // const averageSleep = Math.round(totalDurationHoursAndMinutes / props.sleepEntriesLength);
+
+    // // 3. But we also need to show the remaining minutes
+    // const averageSleepMinRemain = Math.round(totalDurationHoursAndMinutes % props.sleepEntriesLength)
+
+
+
+    // // Change in sleep
+    // const changeDuration = Math.round((thisDaySleepDurationTotal - averageSleep) / thisDaySleepDurationTotal * 100)
+    // const changeScore = Math.round((thisDaySleepScore - averageSleepScore) / thisDaySleepScore * 100)
+
+    return (
+        <div>
+            <h2>Data on your sleep</h2>
+
+            <p>{dayReviewing.replace(/^(\d{4})-(\d{2})-(\d{2})$/, "$2-$3-$1")}</p> */}
+
+            <h3>{/* Add awards */}</h3>
+
+            <table className="datatable">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Change</th>
+                        <th>Last night</th>
+                        <th>Average</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Sleep duration</th>
+                        <td>{changeDuration}%</td>
+                        <td>{thisDaySleepDurationHours} hrs, {thisDaySleepDurationMinutes} min </td>
+                        <td>{averageSleep} hrs, {averageSleepMinRemain} mins</td>
+                    </tr>
+                    <tr>
+                        <th>Sleep score</th>
+                        <td>{changeScore}%</td>
+                        <td>{thisDaySleepScore}</td>
+                        <td>{averageSleepScore}</td>
+                    </tr>
+                    <tr>
+                        <th>Bedtime</th>
+                        <td>-20%</td>
+                        <td>22:59</td>
+                        <td>21:00</td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    )
+}
+
+export default SleepReview;
