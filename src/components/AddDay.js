3853867a698fe11ec92270857947a70a012c0464
William Sulinski <w@williamsulinski.com>
2020-04-28 15:45:04 -0700

    Revert back to React-Hook-Form and Chakra

--- src/components/AddDay.js
+++ src/components/AddDay.js
@@ -4,7 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { addHealth } from '../actions';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { Box, Heading, FormControl, FormLabel, Input, FormErrorMessage, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Stack } from '@chakra-ui/core';
+import { Box, Heading, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/core';
 import { TimePicker } from '@blueprintjs/datetime';
 
 import './AddDay.css';
@@ -14,7 +14,7 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css"
 
 const AddDay = ({ addHealth }) => {
-    const { register, handleSubmit, errors, setValue, watch, control } = useForm();
+    const { register, handleSubmit, errors, setValue } = useForm();
 
     let history = useHistory();
 
@@ -27,17 +27,11 @@ const AddDay = ({ addHealth }) => {
 
     }
 
-    // const watchAllFields = watch();
-
-    // console.log(watchAllFields)
-
     const onSubmit = health => {
         // Uses date from date input to determine the week of the year
-        // health.duration = duration
-        console.log(health)
-        // health.week_of_year = moment(health.summary_date).week()
-        // addHealth(health);
-        // history.push('/data');
+        health.week_of_year = moment(health.summary_date).week()
+        addHealth(health);
+        history.push('/data');
     }
 
     useEffect(() => {
@@ -68,13 +62,8 @@ const AddDay = ({ addHealth }) => {
 
                 <FormControl isInvalid={errors.score_total}>
                     <FormLabel htmlFor='score_total'>Sleep score</FormLabel>
-                    <NumberInput min={0} max={99}>
-                        <NumberInputField type="number" name='score_total' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+                    <Input type="number" name='score_total' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
+
                     <FormErrorMessage>
                         {errors.score_total && <>{errors.score_total.message}</>}
                     </FormErrorMessage>
@@ -93,60 +82,19 @@ const AddDay = ({ addHealth }) => {
                     </FormErrorMessage>
                 </FormControl>
 
-                {/* Prob need a Stack, two number inputs */}
-
-                {/* <div className='form__input'>
-                    <label >Sleep duration</label>
-                    <div className='duration-container'>
-                        <div>
-                            <input
-                                type='number'
-                                name='durationHours'
-                                id='durationHours'
-                                min='0'
-                                max='24'
-                                aria-label='Duration Hours'
-                                ref={register({ required: true })}
-                            />
-                            <label htmlFor='durationHours'>hrs</label>
-                        </div>
-                        <div>
-                            <input
-                                id='durationMinutes'
-                                type='number'
-                                name='durationMinutes'
-                                min='0'
-                                max='59'
-                                aria-label='Duration Minutes'
-                                ref={register}
-                            />
-                            <label htmlFor='durationMinutes'>
-                                mins
-                                    </label>
-                        </div>
-                    </div>
-                </div> */}
-
                 <FormControl >
-
                     <FormLabel htmlFor='readiness'>Sleep duration</FormLabel>
                     <TimePicker selectAllOnFocus='true' onChange={handleTimePicker} />
-
                     <FormErrorMessage>
-                        {errors.readiness && <>{errors.readiness.message}</>}
+                        {/* Need to add error messages for this one */}
                     </FormErrorMessage>
-
                 </FormControl>
 
                 <FormControl isInvalid={errors.readiness}>
                     <FormLabel htmlFor='readiness'>Readiness</FormLabel>
-                    <NumberInput min={0} max={99}>
-                        <NumberInputField type="number" name='readiness' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+
+                    <Input type="number" name='readiness' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
+
                     <FormErrorMessage>
                         {errors.readiness && <>{errors.readiness.message}</>}
                     </FormErrorMessage>
@@ -154,13 +102,7 @@ const AddDay = ({ addHealth }) => {
 
                 <FormControl isInvalid={errors.hrv}>
                     <FormLabel htmlFor='hrv'>HRV Avg</FormLabel>
-                    <NumberInput min={0} max={99}>
-                        <NumberInputField type="number" name='hrv' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+                    <Input type="number" name='hrv' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
                     <FormErrorMessage>
                         {errors.hrv && <>{errors.hrv.message}</>}
                     </FormErrorMessage>
@@ -168,13 +110,7 @@ const AddDay = ({ addHealth }) => {
 
                 <FormControl isInvalid={errors.rhr}>
                     <FormLabel htmlFor='rhr'>RHR Avg</FormLabel>
-                    <NumberInput min={0} max={99}>
-                        <NumberInputField type="number" name='rhr' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+                    <Input type="number" name='rhr' ref={register({ required: 'This field is required.', max: { value: 99, message: 'The maximum score is 99.' }, min: { value: 1, message: 'The minimum score is 1.' } })} />
                     <FormErrorMessage>
                         {errors.rhr && <>{errors.rhr.message}</>}
                     </FormErrorMessage>
@@ -182,143 +118,15 @@ const AddDay = ({ addHealth }) => {
 
                 <FormControl isInvalid={errors.weight}>
                     <FormLabel htmlFor='weight'>Weight (lbs)</FormLabel>
-                    <NumberInput min={0} max={99}>
-                        <NumberInputField type="number" name='weight' ref={register({ max: { value: 500, message: 'The maximum weight is 500.' }, min: { value: 75, message: 'The minimum weight is 75.' } })} />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
+                    <Input type="number" name='weight' ref={register({ max: { value: 500, message: 'The maximum weight is 500.' }, min: { value: 75, message: 'The minimum weight is 75.' } })} />
                     <FormErrorMessage>
                         {errors.weight && <>{errors.weight.message}</>}
                     </FormErrorMessage>
                 </FormControl>
-
-                <div>
-
-
-
-                </div>
-
-
-
                 <input type='submit' />
             </form>
 
-            {/* <form className='adddayform' onSubmit={handleSubmit(onSubmit)}>
-                <div className='form__input'>
-                    <label htmlFor='date'>Date</label>
-                    <input
-                        type='date'
-                        name='summary_date'
-                        ref={register({ required: true })}
-                        aria-label='Date'
-                        className='adddaydate'
-                        defaultValue={new Date().toISOString().substr(0, 10)}
-                    />
-                    {errors.summary_date && <p className='error-message'>This field is required.</p>}
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='score'>Sleep score</label>
-                    <input
-                        type='number'
-                        name='score_total'
-                        min='0'
-                        max='100'
-                        ref={register({ required: true })}
-                    />
-                    {errors.score_total && <p className='error-message'>This field is required.</p>}
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='bedtime'>Bedtime</label>
-                    <input
-                        type='time'
-                        name='bedtime_start'
-                        id='bedtime'
-                        ref={register({ required: true })}
-                    />
-                    {errors.bedtime_start && <p className='error-message'>This field is required.</p>}
-                </div>
-                <div className='form__input'>
-                    <label >Sleep duration</label>
-                    <div className='duration-container'>
-                        <div>
-                            <input
-                                type='number'
-                                name='durationHours'
-                                id='durationHours'
-                                min='0'
-                                max='24'
-                                aria-label='Duration Hours'
-                                ref={register({ required: true })}
-                            />
-                            <label htmlFor='durationHours'>hrs</label>
-                        </div>
-                        <div>
-                            <input
-                                id='durationMinutes'
-                                type='number'
-                                name='durationMinutes'
-                                min='0'
-                                max='59'
-                                aria-label='Duration Minutes'
-                                ref={register}
-                            />
-                            <label htmlFor='durationMinutes'>
-                                mins
-                                    </label>
-                        </div>
-                    </div>
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='readiness'>Readiness</label>
-                    <input
-                        type='number'
-                        name='readiness'
-                        id='readiness'
-                        min='0'
-                        max='100'
-                        ref={register({ required: true })}
-                    />
-                    {errors.readiness && <p className='error-message'>This field is required.</p>}
-
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='hrv'>HRV avg</label>
-                    <input
-                        type='number'
-                        name='hrv'
-                        min='0'
-                        max='100'
-                        ref={register({ required: true })}
-                    />
-                    {errors.hrv && <p className='error-message'>This field is required.</p>}
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='rhr'>RHR avg</label>
-                    <input
-                        type='number'
-                        name='rhr'
-                        min='0'
-                        max='100'
-                        ref={register({ required: true })}
-                    />
-                    {errors.rhr && <p className='error-message'>This field is required.</p>}
-                </div>
-                <div className='form__input'>
-                    <label htmlFor='weight'>Weight (lbs)</label>
-                    <input
-                        type='number'
-                        name='weight'
-                        min='0'
-                        max='500'
-                        ref={register}
-                    />
-                </div>
-                <input type='submit' />
-            </form> */}
-
-        </Box >
+        </Box>
 
     )
 }
